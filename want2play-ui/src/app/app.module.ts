2ecea270f09a39bcfd133655904f6575256a1234
Matias Aguero <mjaguero.work@gmail.com>
2019-11-06 16:54:30 +0100

    Initial Angular APP Angular CLI: 8.3.17 Node: 12.13.0 Angular: 8.2.13

---
+++ want2play-ui/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LoginComponent } from './login/login.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    TopNavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
